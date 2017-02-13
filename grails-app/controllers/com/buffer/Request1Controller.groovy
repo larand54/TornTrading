5b699c9a80e9fa34f4a7efaf6f703e30df731a4e
Lars-Göran Andersson <larand54@yahoo.se>
2017-02-13 16:49:57 +0100

    Fått Login att fungera.

--- grails-app/controllers/com/buffer/Request1Controller.groovy
+++ grails-app/controllers/com/buffer/Request1Controller.groovy
@@ -2,12 +2,16 @@ package com.buffer
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
+import grails.plugin.springsecurity.annotation.Secured
 
 @Transactional(readOnly = true)
+
+@Secured('ROLE_ADMIN')
 class Request1Controller {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
+    @Secured('ROLE_USER')
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond Request1.list(params), model:[request1Count: Request1.count()]
